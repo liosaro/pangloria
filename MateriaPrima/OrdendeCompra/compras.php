56f77980c44153901b7027fea7635a31f757706a
Julio Flores <julitoflores1987@gmail.com>
2013-05-01 12:37:28 -0600

    acomodacion carpeta modulos nuevos
    se crearon las carpeta para los modulos nuevos del sistema en este
    cambio se creo dentro de la carpeta materia prima  la carpeta de
    ordendeCompra, medidaspeso,

--- MateriaPrima/OrdendeCompra/compras.php
+++ MateriaPrima/OrdendeCompra/compras.php
@@ -3,7 +3,7 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 <title>Documento sin título</title>
-<link href="../style.css" rel="stylesheet" type="text/css" />
+<link href="../../style.css" rel="stylesheet" type="text/css" />
 </head>
 
 <body>
