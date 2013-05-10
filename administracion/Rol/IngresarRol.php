3ebf57999c5903edfe2211c1d0328b3eac6b8262
Ana Marielos Leal Acosta <lealmarielos@gmail.com>
2013-05-09 18:27:15 -0600

    Ingreso nodulo de roles
    se agrego el modulo de roles haciendo falta el de eliminar

---
+++ administracion/Rol/IngresarRol.php
@@ -0,0 +1,95 @@
+<?php require_once('../../Connections/basepangloria.php'); ?>
+<?php
+if (!function_exists("GetSQLValueString")) {
+function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDefinedValue = "") 
+{
+  if (PHP_VERSION < 6) {
+    $theValue = get_magic_quotes_gpc() ? stripslashes($theValue) : $theValue;
+  }
+
+  $theValue = function_exists("mysql_real_escape_string") ? mysql_real_escape_string($theValue) : mysql_escape_string($theValue);
+
+  switch ($theType) {
+    case "text":
+      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+      break;    
+    case "long":
+    case "int":
+      $theValue = ($theValue != "") ? intval($theValue) : "NULL";
+      break;
+    case "double":
+      $theValue = ($theValue != "") ? doubleval($theValue) : "NULL";
+      break;
+    case "date":
+      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+      break;
+    case "defined":
+      $theValue = ($theValue != "") ? $theDefinedValue : $theNotDefinedValue;
+      break;
+  }
+  return $theValue;
+}
+}
+
+$editFormAction = $_SERVER['PHP_SELF'];
+if (isset($_SERVER['QUERY_STRING'])) {
+  $editFormAction .= "?" . htmlentities($_SERVER['QUERY_STRING']);
+}
+
+if ((isset($_POST["MM_insert"])) && ($_POST["MM_insert"] == "form2")) {
+  $insertSQL = sprintf("INSERT INTO CATROL (IDROL, DESCRIPCION) VALUES (%s, %s)",
+                       GetSQLValueString($_POST['IDROL'], "int"),
+                       GetSQLValueString($_POST['DESCRIPCION'], "text"));
+
+  mysql_select_db($database_basepangloria, $basepangloria);
+  $Result1 = mysql_query($insertSQL, $basepangloria) or die(mysql_error());
+}
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Documento sin título</title>
+<style type="text/css">
+body {
+	margin-left: 0px;
+	margin-top: 0px;
+	margin-right: 0px;
+	margin-bottom: 0px;
+}
+</style>
+</head>
+
+<body>
+<table width="820" border="0">
+  <tr>
+    <td><form id="form1" name="form1" method="post" action="">
+    </form>
+      <form action="<?php echo $editFormAction; ?>" method="post" name="form2" id="form2">
+        <table width="100%" border="0">
+          <tr>
+            <td colspan="4" align="center">Ingresar Rol</td>
+          </tr>
+          <tr>
+            <td>Id Rol:</td>
+            <td><input name="IDROL" type="text" disabled="disabled" value="" size="32" /></td>
+            <td>DESCRIPCION:</td>
+            <td><input type="text" name="DESCRIPCION" value="" size="32" /></td>
+          </tr>
+          <tr>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+          </tr>
+          <tr>
+            <td colspan="4" align="center"><input type="submit" value="Insertar registro" /></td>
+          </tr>
+        </table>
+        <input type="hidden" name="MM_insert" value="form2" />
+      </form>
+    <p>&nbsp;</p></td>
+  </tr>
+</table>
+</body>
+</html>
