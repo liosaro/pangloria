1dfdb4fd71e45f113b0beb77c2d93ecefb7066f7
Ana Marielos Leal Acosta <lealmarielos@gmail.com>
2013-05-11 10:45:32 -0600

    modificacion de eliminar rol

--- administracion/Rol/eliminarRol.php
+++ administracion/Rol/eliminarRol.php
@@ -51,14 +51,6 @@ if ((isset($_GET['IDROL'])) && ($_GET['IDROL'] != "")) {
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>Documento sin título</title>
-<style type="text/css">
-body {
-	margin-left: 0px;
-	margin-top: 0px;
-	margin-right: 0px;
-	margin-bottom: 0px;
-}
-</style>
 </head>
 
 <body>
