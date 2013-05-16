4919774744e5d2d9619b895b19d7714dbdff19ec
Claudia Mendez <claudita@hotmail.com>
2013-05-16 10:15:49 -0500

    Formulario para compra
    para introducir una compra

--- MateriaPrima/Compra/ingreso.php
+++ MateriaPrima/Compra/ingreso.php
@@ -51,6 +51,42 @@ if ((isset($_POST["MM_insert"])) && ($_POST["MM_insert"] == "form2")) {
   $Result1 = mysql_query($insertSQL, $basepangloria) or die(mysql_error());
 }
 
+if ((isset($_POST["MM_insert"])) && ($_POST["MM_insert"] == "form3")) {
+  $insertSQL = sprintf("INSERT INTO TRNDETALLECOMPRA (IDCOMPRA, IDUNIDAD, ID_DETENCCOM, CANTIDADMATPRIMA, MATERIAPRIMA, PRECIOUNIDAD, PRECIOTOTAL, DESCUENTO, SUBTOTAL, IVA, TOTAL) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
+                       GetSQLValueString($_POST['IDCOMPRA'], "int"),
+                       GetSQLValueString($_POST['IDUNIDAD'], "int"),
+                       GetSQLValueString($_POST['ID_DETENCCOM'], "int"),
+                       GetSQLValueString($_POST['CANTIDADMATPRIMA'], "int"),
+                       GetSQLValueString($_POST['MATERIAPRIMA'], "int"),
+                       GetSQLValueString($_POST['PRECIOUNIDAD'], "double"),
+                       GetSQLValueString($_POST['PRECIOTOTAL'], "double"),
+                       GetSQLValueString($_POST['DESCUENTO'], "double"),
+                       GetSQLValueString($_POST['SUBTOTAL'], "double"),
+                       GetSQLValueString($_POST['IVA'], "double"),
+                       GetSQLValueString($_POST['TOTAL'], "double"));
+
+  mysql_select_db($database_basepangloria, $basepangloria);
+  $Result1 = mysql_query($insertSQL, $basepangloria) or die(mysql_error());
+}
+
+if ((isset($_POST["MM_insert"])) && ($_POST["MM_insert"] == "form4")) {
+  $insertSQL = sprintf("INSERT INTO TRNDETALLECOMPRA (IDCOMPRA, IDUNIDAD, ID_DETENCCOM, CANTIDADMATPRIMA, MATERIAPRIMA, PRECIOUNIDAD, PRECIOTOTAL, DESCUENTO, SUBTOTAL, IVA, TOTAL) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
+                       GetSQLValueString($_POST['IDCOMPRA'], "int"),
+                       GetSQLValueString($_POST['IDUNIDAD'], "int"),
+                       GetSQLValueString($_POST['ID_DETENCCOM'], "int"),
+                       GetSQLValueString($_POST['CANTIDADMATPRIMA'], "int"),
+                       GetSQLValueString($_POST['MATERIAPRIMA'], "int"),
+                       GetSQLValueString($_POST['PRECIOUNIDAD'], "double"),
+                       GetSQLValueString($_POST['PRECIOTOTAL'], "double"),
+                       GetSQLValueString($_POST['DESCUENTO'], "double"),
+                       GetSQLValueString($_POST['SUBTOTAL'], "double"),
+                       GetSQLValueString($_POST['IVA'], "double"),
+                       GetSQLValueString($_POST['TOTAL'], "double"));
+
+  mysql_select_db($database_basepangloria, $basepangloria);
+  $Result1 = mysql_query($insertSQL, $basepangloria) or die(mysql_error());
+}
+
 mysql_select_db($database_basepangloria, $basepangloria);
 $query_Recordset1 = "SELECT IDORDEN FROM TRNENCAORDCOMPRA";
 $Recordset1 = mysql_query($query_Recordset1, $basepangloria) or die(mysql_error());
@@ -70,7 +106,8 @@ $totalRows_Recordset1 = mysql_num_rows($Recordset1);
     <tr>
       <td><table width="99%" border="1">
         <tr>
-          <td colspan="8" align="center" bgcolor="#999999"><h1>Formulario para el Ingreso de Compra</h1></td>
+          <td colspan="8" align="center" bgcolor="#999999"><h1>&nbsp;</h1>
+            <h1>Formulario para el Ingreso de Compra</h1></td>
           </tr>
         <tr>
           <td align="center">ID_DETENCCOM</td>
@@ -161,22 +198,180 @@ $totalRows_Recordset1 = mysql_num_rows($Recordset1);
           <td><input type="submit" value="Insertar registro" /></td>
         </tr>
         <tr>
-          <td>&nbsp;</td>
-          <td>&nbsp;</td>
-          <td>&nbsp;</td>
-          <td>&nbsp;</td>
-          <td>&nbsp;</td>
-          <td>&nbsp;</td>
-          <td>&nbsp;</td>
-          <td>&nbsp;</td>
+          <td colspan="8" align="center" bgcolor="#999999"><h1>Dellate de la Compra</h1></td>
         </tr>
-    </table></td>
+      </table></td>
     </tr>
   </table>
 </form>
-<form action="<?php echo $editFormAction; ?>" method="post" name="form2" id="form2">
-  <input type="hidden" name="MM_insert" value="form2" />
+<form action="<?php echo $editFormAction; ?>" method="post" name="form3" id="form3">
+  <table width="99%" border="1">
+    <tr>
+      <td width="8%">IDCOMPRA:</td>
+      <td width="18%"><input type="text" name="IDCOMPRA" value="" size="32" /></td>
+      <td width="8%">IDUNIDAD:</td>
+      <td width="10%"><select name="IDUNIDAD">
+        <option value="menuitem1" >[ Etiqueta ]</option>
+        <option value="menuitem2" >[ Etiqueta ]</option>
+      </select></td>
+      <td width="12%">ID_DETENCCOM</td>
+      <td width="10%"><select name="ID_DETENCCOM2">
+        <option value="menuitem1" >[ Etiqueta ]</option>
+        <option value="menuitem2" >[ Etiqueta ]</option>
+      </select></td>
+      <td width="16%">MATERIAPRIMA</td>
+      <td width="18%"><select name="MATERIAPRIMA">
+        <option value="menuitem1" >[ Etiqueta ]</option>
+        <option value="menuitem2" >[ Etiqueta ]</option>
+      </select></td>
+    </tr>
+    <tr>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+    </tr>
+    <tr>
+      <td>CANTIDADMATPRIMA:</td>
+      <td><input type="text" name="CANTIDADMATPRIMA" value="" size="32" /></td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+    </tr>
+    <tr>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+    </tr>
+    <tr>
+      <td>PRECIOUNIDAD</td>
+      <td><input type="text" name="PRECIOUNIDAD" value="" size="32" /></td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+    </tr>
+    <tr>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+    </tr>
+    <tr>
+      <td>PRECIOTOTAL</td>
+      <td><input type="text" name="PRECIOTOTAL" value="" size="32" /></td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+    </tr>
+    <tr>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+    </tr>
+    <tr>
+      <td>DESCUENTO</td>
+      <td><input type="text" name="DESCUENTO" value="" size="32" /></td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+    </tr>
+    <tr>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+    </tr>
+    <tr>
+      <td>SUBTOTAL</td>
+      <td><input type="text" name="SUBTOTAL" value="" size="32" /></td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+    </tr>
+    <tr>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+    </tr>
+    <tr>
+      <td>IVA:</td>
+      <td><input type="text" name="IVA" value="" size="32" /></td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+    </tr>
+    <tr>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+    </tr>
+    <tr>
+      <td>TOTAL</td>
+      <td><input type="text" name="TOTAL" value="" size="32" /></td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td><input type="submit" value="Insertar registro" /></td>
+    </tr>
+  </table>
+  <input type="hidden" name="MM_insert" value="form3" />
 </form>
+<form action="<?php echo $editFormAction; ?>" method="post" name="form4" id="form4">
+  <input type="hidden" name="MM_insert" value="form4" />
+</form>
+<p>&nbsp;</p>
 <p>&nbsp;</p>
 </body>
 </html>
