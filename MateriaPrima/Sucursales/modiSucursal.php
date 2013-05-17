c3c957d925ff76f8c3864b9add3386e83e7775fb
Ana Marielos Leal Acosta <lealmarielos@gmail.com>
2013-05-16 22:16:16 -0600

    modificacion sucursal

--- MateriaPrima/Sucursales/modiSucursal.php
+++ MateriaPrima/Sucursales/modiSucursal.php
@@ -3,6 +3,14 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>Documento sin título</title>
+<style type="text/css">
+body {
+	margin-left: 0px;
+	margin-top: 0px;
+	margin-right: 0px;
+	margin-bottom: 0px;
+}
+</style>
 </head>
 
 <body>
