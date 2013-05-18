7abf628c7b74d6f83335332ad96d7cfa951b7c57
Elizabeth1706 <apolito1981>
2013-05-18 01:58:34 -0600

    ingreso empleados
    se hizo el ingreso de empleados

---
+++ MateriaPrima/empleado/ingresarEmpleado.php
@@ -0,0 +1,161 @@
+<?php require_once('../../Connections/basepangloria.php'); ?>
+<?php
+if (!function_exists("GetSQLValueString")) {
+function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDefinedValue = "") 
+{
+  if (PHP_VERSION < 6) {
+    $theValue = get_magic_quotes_gpc() ? stripslashes($theValue) : $theValue;
+  }
+
+  $theValue = function_exists("mysql_real_escape_string") ? mysql_real_escape_string($theValue) : mysql_escape_string($theValue);
+
+  switch ($theType) {
+    case "text":
+      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+      break;    
+    case "long":
+    case "int":
+      $theValue = ($theValue != "") ? intval($theValue) : "NULL";
+      break;
+    case "double":
+      $theValue = ($theValue != "") ? doubleval($theValue) : "NULL";
+      break;
+    case "date":
+      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+      break;
+    case "defined":
+      $theValue = ($theValue != "") ? $theDefinedValue : $theNotDefinedValue;
+      break;
+  }
+  return $theValue;
+}
+}
+
+$editFormAction = $_SERVER['PHP_SELF'];
+if (isset($_SERVER['QUERY_STRING'])) {
+  $editFormAction .= "?" . htmlentities($_SERVER['QUERY_STRING']);
+}
+
+if ((isset($_POST["MM_insert"])) && ($_POST["MM_insert"] == "form1")) {
+  $insertSQL = sprintf("INSERT INTO CATEMPLEADO (IDEMPLEADO, IDDEPTO, IDCARGO, IDSUCURSAL, NOMBREEMPLEADO, EDADEMPLEADO, DIRECCION, NIT, NUP, NSEGURO, DUI, CUENTABANCARIA, CORREOEMPLEADO, MOVILEMPLEADO, FIJO) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
+                       GetSQLValueString($_POST['IDEMPLEADO'], "int"),
+                       GetSQLValueString($_POST['IDDEPTO'], "int"),
+                       GetSQLValueString($_POST['IDCARGO'], "int"),
+                       GetSQLValueString($_POST['IDSUCURSAL'], "int"),
+                       GetSQLValueString($_POST['NOMBREEMPLEADO'], "text"),
+                       GetSQLValueString($_POST['EDADEMPLEADO'], "date"),
+                       GetSQLValueString($_POST['DIRECCION'], "text"),
+                       GetSQLValueString($_POST['NIT'], "text"),
+                       GetSQLValueString($_POST['NUP'], "text"),
+                       GetSQLValueString($_POST['NSEGURO'], "text"),
+                       GetSQLValueString($_POST['DUI'], "text"),
+                       GetSQLValueString($_POST['CUENTABANCARIA'], "text"),
+                       GetSQLValueString($_POST['CORREOEMPLEADO'], "text"),
+                       GetSQLValueString($_POST['MOVILEMPLEADO'], "text"),
+                       GetSQLValueString($_POST['FIJO'], "text"));
+
+  mysql_select_db($database_basepangloria, $basepangloria);
+  $Result1 = mysql_query($insertSQL, $basepangloria) or die(mysql_error());
+}
+
+mysql_select_db($database_basepangloria, $basepangloria);
+$query_ingreemple = "SELECT * FROM CATEMPLEADO ORDER BY IDEMPLEADO ASC";
+$ingreemple = mysql_query($query_ingreemple, $basepangloria) or die(mysql_error());
+$row_ingreemple = mysql_fetch_assoc($ingreemple);
+$totalRows_ingreemple = mysql_num_rows($ingreemple);
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Documento sin título</title>
+</head>
+
+<body>
+<form action="<?php echo $editFormAction; ?>" method="post" name="form1" id="form1">
+  <p>&nbsp;</p>
+  <table align="center">
+    <tr valign="baseline">
+      <td nowrap="nowrap" align="right">IDEMPLEADO:</td>
+      <td><input type="text" name="IDEMPLEADO" value="" size="32" /></td>
+    </tr>
+    <tr valign="baseline">
+      <td nowrap="nowrap" align="right">IDDEPTO:</td>
+      <td><select name="IDDEPTO">
+        <option value="menuitem1" >[ Etiqueta ]</option>
+        <option value="menuitem2" >[ Etiqueta ]</option>
+      </select></td>
+    </tr>
+    <tr valign="baseline">
+      <td nowrap="nowrap" align="right">IDCARGO:</td>
+      <td><select name="IDCARGO">
+        <option value="menuitem1" >[ Etiqueta ]</option>
+        <option value="menuitem2" >[ Etiqueta ]</option>
+      </select></td>
+    </tr>
+    <tr valign="baseline">
+      <td nowrap="nowrap" align="right">IDSUCURSAL:</td>
+      <td><select name="IDSUCURSAL">
+        <option value="menuitem1" >[ Etiqueta ]</option>
+        <option value="menuitem2" >[ Etiqueta ]</option>
+      </select></td>
+    </tr>
+    <tr valign="baseline">
+      <td nowrap="nowrap" align="right">NOMBREEMPLEADO:</td>
+      <td><input type="text" name="NOMBREEMPLEADO" value="" size="32" /></td>
+    </tr>
+    <tr valign="baseline">
+      <td nowrap="nowrap" align="right">EDADEMPLEADO:</td>
+      <td><input type="text" name="EDADEMPLEADO" value="" size="32" /></td>
+    </tr>
+    <tr valign="baseline">
+      <td nowrap="nowrap" align="right">DIRECCION:</td>
+      <td><input type="text" name="DIRECCION" value="" size="32" /></td>
+    </tr>
+    <tr valign="baseline">
+      <td nowrap="nowrap" align="right">NIT:</td>
+      <td><input type="text" name="NIT" value="" size="32" /></td>
+    </tr>
+    <tr valign="baseline">
+      <td nowrap="nowrap" align="right">NUP:</td>
+      <td><input type="text" name="NUP" value="" size="32" /></td>
+    </tr>
+    <tr valign="baseline">
+      <td nowrap="nowrap" align="right">NSEGURO:</td>
+      <td><input type="text" name="NSEGURO" value="" size="32" /></td>
+    </tr>
+    <tr valign="baseline">
+      <td nowrap="nowrap" align="right">DUI:</td>
+      <td><input type="text" name="DUI" value="" size="32" /></td>
+    </tr>
+    <tr valign="baseline">
+      <td nowrap="nowrap" align="right">CUENTABANCARIA:</td>
+      <td><input type="text" name="CUENTABANCARIA" value="" size="32" /></td>
+    </tr>
+    <tr valign="baseline">
+      <td nowrap="nowrap" align="right">CORREOEMPLEADO:</td>
+      <td><input type="text" name="CORREOEMPLEADO" value="" size="32" /></td>
+    </tr>
+    <tr valign="baseline">
+      <td nowrap="nowrap" align="right">MOVILEMPLEADO:</td>
+      <td><input type="text" name="MOVILEMPLEADO" value="" size="32" /></td>
+    </tr>
+    <tr valign="baseline">
+      <td nowrap="nowrap" align="right">FIJO:</td>
+      <td><input type="text" name="FIJO" value="" size="32" /></td>
+    </tr>
+    <tr valign="baseline">
+      <td nowrap="nowrap" align="right">&nbsp;</td>
+      <td><input type="submit" value="Insertar registro" /></td>
+    </tr>
+  </table>
+  <p>
+    <input type="hidden" name="MM_insert" value="form1" />
+  </p>
+</form>
+<p>&nbsp;</p>
+</body>
+</html>
+<?php
+mysql_free_result($ingreemple);
+?>
