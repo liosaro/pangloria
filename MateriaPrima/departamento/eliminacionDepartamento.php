0ad3c3294360d333185db20abb8029b54d2be8ef
lande3001 <lande3001@gmail.com>
2013-05-18 22:51:31 -0300

    eliminando archivo dañados
    archivos dañados de errores

--- MateriaPrima/departamento/eliminacionDepartamento.php
+++ MateriaPrima/departamento/eliminacionDepartamento.php
@@ -31,163 +31,33 @@ function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDe
 }
 }
 
-$currentPage = $_SERVER["PHP_SELF"];
-
-$maxRows_consultaempresa = 5;
-$pageNum_consultaempresa = 0;
-if (isset($_GET['pageNum_consultaempresa'])) {
-  $pageNum_consultaempresa = $_GET['pageNum_consultaempresa'];
-}
-$startRow_consultaempresa = $pageNum_consultaempresa * $maxRows_consultaempresa;
-
 mysql_select_db($database_basepangloria, $basepangloria);
-$query_consultaempresa = "SELECT * FROM CATDEPARTAMENEMPRESA ORDER BY IDDEPTO DESC";
-$query_limit_consultaempresa = sprintf("%s LIMIT %d, %d", $query_consultaempresa, $startRow_consultaempresa, $maxRows_consultaempresa);
-$consultaempresa = mysql_query($query_limit_consultaempresa, $basepangloria) or die(mysql_error());
-$row_consultaempresa = mysql_fetch_assoc($consultaempresa);
-
-if (isset($_GET['totalRows_consultaempresa'])) {
-  $totalRows_consultaempresa = $_GET['totalRows_consultaempresa'];
-} else {
-  $all_consultaempresa = mysql_query($query_consultaempresa);
-  $totalRows_consultaempresa = mysql_num_rows($all_consultaempresa);
-}
-$totalPages_consultaempresa = ceil($totalRows_consultaempresa/$maxRows_consultaempresa ) -1;
-$pageNum_consultaempresa = 0;
-if (isset($_GET['pageNum_consultaempresa'])) {
-  $pageNum_consultaempresa = $_GET['pageNum_consultaempresa'];
-}
-$startRow_consultaempresa = $pageNum_consultaempresa * $maxRows_consultaempresa;
-
-mysql_select_db($database_basepangloria, $basepangloria);
-$query_consultaempresa = "SELECT * FROM CATDEPARTAMENEMPRESA ORDER BY IDDEPTO DESC";
-$query_limit_consultaempresa = sprintf("%s LIMIT %d, %d", $query_consultaempresa, $startRow_consultaempresa, $maxRows_consultaempresa);
-$consultaempresa = mysql_query($query_limit_consultaempresa, $basepangloria) or die(mysql_error());
-$row_consultaempresa = mysql_fetch_assoc($consultaempresa);
-
-if (isset($_GET['totalRows_consultaempresa'])) {
-  $totalRows_consultaempresa = $_GET['totalRows_consultaempresa'];
-} else {
-  $all_consultaempresa = mysql_query($query_consultaempresa);
-  $totalRows_consultaempresa = mysql_num_rows($all_consultaempresa);
-}
-$totalPages_consultaempresa = ceil($totalRows_consultaempresa/$maxRows_consultaempresa)-1;$maxRows_consultaempresa = -1;
-$pageNum_consultaempresa = 0;
-if (isset($_GET['pageNum_consultaempresa'])) {
-  $pageNum_consultaempresa = $_GET['pageNum_consultaempresa'];
-}
-$startRow_consultaempresa = $pageNum_consultaempresa * $maxRows_consultaempresa;
-
-mysql_select_db($database_basepangloria, $basepangloria);
-$query_consultaempresa = "SELECT * FROM CATDEPARTAMENEMPRESA ORDER BY IDDEPTO DESC";
-$query_limit_consultaempresa = sprintf("%s LIMIT %d, %d", $query_consultaempresa, $startRow_consultaempresa, $maxRows_consultaempresa);
-$consultaempresa = mysql_query($query_limit_consultaempresa, $basepangloria) or die(mysql_error());
-$row_consultaempresa = mysql_fetch_assoc($consultaempresa);
-
-if (isset($_GET['totalRows_consultaempresa'])) {
-  $totalRows_consultaempresa = $_GET['totalRows_consultaempresa'];
-} else {
-  $all_consultaempresa = mysql_query($query_consultaempresa);
-  $totalRows_consultaempresa = mysql_num_rows($all_consultaempresa);
-}
-$totalPages_consultaempresa = ceil($totalRows_consultaempresa/$maxRows_consultaempresa)-1;
-
-$queryString_consultaempresa = "";
-if (!empty($_SERVER['QUERY_STRING'])) {
-  $params = explode("&", $_SERVER['QUERY_STRING']);
-  $newParams = array();
-  foreach ($params as $param) {
-    if (stristr($param, "pageNum_consultaempresa") == false && 
-        stristr($param, "totalRows_consultaempresa") == false) {
-      array_push($newParams, $param);
-    }
-  }
-  if (count($newParams) != 0) {
-    $queryString_consultaempresa = "&" . htmlentities(implode("&", $newParams));
-  }
-}
-$queryString_consultaempresa = sprintf("&totalRows_consultaempresa=%d%s", $totalRows_consultaempresa, $queryString_consultaempresa);
-
-$queryString_consultaempresa = "";
-if (!empty($_SERVER['QUERY_STRING'])) {
-  $params = explode("&", $_SERVER['QUERY_STRING']);
-  $newParams = array();
-  foreach ($params as $param) {
-    if (stristr($param, "pageNum_consultaempresa") == false && 
-        stristr($param, "totalRows_consultaempresa") == false) {
-      array_push($newParams, $param);
-    }
-  }
-  if (count($newParams) != 0) {
-    $queryString_consultaempresa = "&" . htmlentities(implode("&", $newParams));
-  }
-}
-$queryString_consultaempresa = sprintf("&totalRows_consultaempresa=%d%s", $totalRows_consultaempresa, $queryString_consultaempresa);
+$query_departamento = "SELECT * FROM CATDEPARTAMENEMPRESA";
+$departamento = mysql_query($query_departamento, $basepangloria) or die(mysql_error());
+$row_departamento = mysql_fetch_assoc($departamento);
+$totalRows_departamento = mysql_num_rows($departamento);mysql_select_db($database_basepangloria, $basepangloria);
+$query_departamento = "SELECT * FROM CATDEPARTAMENEMPRESA ORDER BY IDDEPTO DESC";
+$departamento = mysql_query($query_departamento, $basepangloria) or die(mysql_error());
+$row_departamento = mysql_fetch_assoc($departamento);
+$totalRows_departamento = mysql_num_rows($departamento);
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Control de Empleados</title>
-<link href="../../style.css" rel="stylesheet" type="text/css" />
-<script language="JavaScript">
-function aviso(url){
-if (!confirm("ALERTA!! va a proceder a eliminar este registro, si desea eliminarlo de click en ACEPTAR\n de lo contrario de click en CANCELAR.")) {
-return false;
-}
-else {
-document.location = url;
-return true;
-}
-}
-</script>
+<title>Documento sin título</title>
 </head>
 
 <body>
-<div class="content" id="contenidoadminphp2">
-  <table width="844" border="0" cellspacing="0" cellpadding="0">
-    <tr>
-      <td width="844" align="center"><div class="cont">
-        <form action="filtroeliminarDepar.php" method="post" name="enviodepa" target="modidepa" id="enviodepa">
-          <table width="1026" border="0" cellspacing="0" cellpadding="0">
-            <tr>
-              <td width="800" colspan="6" align="left">&nbsp;
-                <iframe src="filtroeliminaproducto.php" name="modiprodu" width="830" height="200" align="middle" scrolling="Auto" frameborder="0" id="modiprodu"></iframe>
-<p><a href="<?php printf("%s?pageNum_consultaempresa=%d%s", $currentPage, 0, $queryString_consultaempresa); ?>"><img src="../../imagenes/icono/Back-32.png" alt="" width="32" height="32" /></a><a href="<?php printf("%s?pageNum_consultaempresa=%d%s", $currentPage, max(0, $pageNum_consultaempresa - 1), $queryString_consultaempresa); ?>"><img src="../../imagenes/icono/Backward-32.png" alt="" width="32" height="32" /></a><a href="<?php printf("%s?pageNum_consultaempresa=%d%s", $currentPage, min($totalPages_consultaempresa, $pageNum_consultaempresa + 1), $queryString_consultaempresa); ?>"><img src="../../imagenes/icono/Forward-32.png" alt="" width="32" height="32" /></a><a href="<?php printf("%s?pageNum_consultaempresa=%d%s", $currentPage, $totalPages_consultaempresa, $queryString_consultaempresa); ?>"><img src="../../imagenes/icono/Next-32.png" alt="" width="32" height="32" /></a></p>
-                <table width="830" border="1">
-                  <tr>
-                    <td colspan="2"><input type="text" name="root" id="root" />
-                      <input type="submit" name="filtrar" id="filtrar" value="Filtrar" /></td>
-                    <td>&nbsp;</td>
-                    <td>&nbsp;</td>
-                    </tr>
-                  <tr>
-                    <td>Modificar</td>
-                    <td>Id del Departamento</td>
-                    <td>Nombre del Departamento</td>
-                    <td>Numero de Telefono</td>
-                    </tr>
-                  <?php do { ?>
-                    <tr>
-                      <td><a href="javascript:;" onclick="aviso('eliminarDepartamento.php?root=<?php echo $row_filtradodepartamento['IDDEPTO']; ?>; return false;">Eliminar</a></td>
-                      <td><?php echo $row_consultaempresa['IDDEPTO']; ?></td>
-                      <td><?php echo $row_consultaempresa['DEPARTAMENTO']; ?></td>
-                      <td><?php echo $row_consultaempresa['NUMEROTELEFONO']; ?></td>
-                      </tr>
-                    <?php } while ($row_consultaempresa = mysql_fetch_assoc($consultaempresa)); ?>
-                </table></td>
-            </tr>
-            </table>
-        </form>
-      </div></td>
-    </tr>
-  </table>
-</div>
-<p>&nbsp;</p>
+<table width="820" border="0">
+  <tr>
+    <td><form id="form1" name="form1" method="post" action=""><u><iframe src="" width="820" height="400" scrolling="auto"></iframe></u>
+      <p>&nbsp;</p>
+    </form></td>
+  </tr>
+</table>
 </body>
 </html>
 <?php
-mysql_free_result($consultaempresa);
-
-mysql_free_result($consultaProducto);
+mysql_free_result($departamento);
 ?>
