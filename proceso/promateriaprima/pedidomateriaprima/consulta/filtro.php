1439382b555f2d286adf3abf33d4500609972ee7
Claudia Mendez <claudita@hotmail.com>
2013-05-24 20:44:30 -0500

    mdificando

---
+++ proceso/promateriaprima/pedidomateriaprima/consulta/filtro.php
@@ -0,0 +1,107 @@
+<?php require_once('../../../../Connections/basepangloria.php'); ?>
+<?php
+if (!function_exists("GetSQLValueString")) {
+function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDefinedValue = "") 
+{
+  if (PHP_VERSION < 6) {
+    $theValue = get_magic_quotes_gpc() ? stripslashes($theValue) : $theValue;
+  }
+
+  $theValue = function_exists("mysql_real_escape_string") ? mysql_real_escape_string($theValue) : mysql_escape_string($theValue);
+
+  switch ($theType) {
+    case "text":
+      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+      break;    
+    case "long":
+    case "int":
+      $theValue = ($theValue != "") ? intval($theValue) : "NULL";
+      break;
+    case "double":
+      $theValue = ($theValue != "") ? doubleval($theValue) : "NULL";
+      break;
+    case "date":
+      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+      break;
+    case "defined":
+      $theValue = ($theValue != "") ? $theDefinedValue : $theNotDefinedValue;
+      break;
+  }
+  return $theValue;
+}
+}
+
+$maxRows_consulta = 10;
+$pageNum_consulta = 0;
+if (isset($_GET['pageNum_consulta'])) {
+  $pageNum_consulta = $_GET['pageNum_consulta'];
+}
+$startRow_consulta = $pageNum_consulta * $maxRows_consulta;
+mysql_select_db($database_basepangloria, $basepangloria);
+
+ if (isset($_GET['fechai']) && isset($_GET['fechaf'])) {
+
+        $fechainicio = '"' . $_GET['fechai'] . '"';
+
+        $fechafin = '"' . $_GET['fechaf'] . '"';
+
+        $row_consulta = "SELECT * FROM TRNENCABEZADOPEDMATPRI WHERE FECHA BETWEEN $fechainicio AND $fechafin ORDER BY FECHA DESC";
+
+        $result_buscar = mysql_query($row_consulta);
+
+        if (!$result_buscar) {
+			echo "$fechafin";
+			echo "$fechainicio";
+            echo "No pudo ejecutarse satisfactoriamente la consulta ($sql) " .
+            "en la BD: " . mysql_error();
+            //Finalizo la aplicación
+            exit;
+        }
+ }
+
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Documento sin título</title>
+<link href="../../../../css/forms.css" rel="stylesheet" type="text/css" />
+<link href="http://netdna.bootstrapcdn.com/twitter-bootstrap/2.2.2/css/bootstrap-combined.min.css" rel="stylesheet">
+<link rel="stylesheet" type="text/css" media="screen" href="http://tarruda.github.com/bootstrap-datetimepicker/assets/css/bootstrap-datetimepicker.min.css">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+</head>
+
+<body>
+<table width="820" border="0">
+  <tr>
+    <td>
+    <div class="input-prepend">
+        <h5 style="margin-left: 15px ;">Filtrar por Fecha</h5>
+            <span style="margin-left: 15px ;" class="add-on">Fecha Inicio</span><input id="fechai" type="date">    
+            <span style="margin-left: 15px ;" class="add-on">Fecha Fin</span><input id="fechaf" type="date">
+            <button style="margin-left: 15px ;" class="btn btn-warning" onclick="window.open('?fechai='+fechai.value+'&fechaf='+fechaf.value+'','_self');">Buscar</button>       
+      </div>
+    </td>
+  </tr>
+</table>
+<table width="820" border="1" cellpadding="0" cellspacing="0">
+  <tr>
+    <td>Pedido de Materia Prima No.</td>
+    <td>Orden de Produccion No.</td>
+    <td>Fecha</td>
+     <td>consultar</td>
+  </tr>
+  <?php do { ?>
+    <tr>
+      <td><?php echo $result['ID_ENCAPEDIDO']; ?></td>
+      <td><?php echo $result['IDORDENPRODUCCION']; ?></td>
+      <td><?php echo $result['FECHA']; ?></td>
+      <td align="center"><a href="consulta.php?enca=<?php echo $result['ID_ENCAPEDIDO']; ?>" target="_self"><img src="../../../../imagenes/icono/Invoice-256.png" width="32" height="32" onclick="" /></a></td>
+    </tr>
+    <?php } while ($result = mysql_fetch_assoc($result_buscar)); ?>
+</table>
+</body>
+</html>
+<?php
+mysql_free_result($consulta);
+?>
