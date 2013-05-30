47b509514c71505f1132c8473dfed805128cd744
Claudia Mendez <claudita@hotmail.com>
2013-05-30 06:11:35 -0500

    consultar compra
    nuevo formulario, diseño

---
+++ MateriaPrima/Compra/consutalcompra.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Documento sin título</title>
+<style type="text/css">
+body {
+	margin-left: 0px;
+	margin-top: 0px;
+	margin-right: 0px;
+	margin-bottom: 0px;
+}
+</style>
+hhh
+</head>
+
+<body>
+<table width="820">
+  <tr>
+    <td align="center" bgcolor="#999999"><h1>Formulario de Consulta de Compras</h1></td>
+  </tr>
+  <tr>
+    <td><p>&nbsp;</p>
+    <p>
+      <label for="textfield"></label>
+      <input type="text" name="textfield" id="textfield" />
+      <input type="submit" name="enviar" id="enviar" value="Enviar" />
+    </p>
+    <table width="820">
+      <tr>
+        <td width="118">Consultar por</td>
+        <td width="690"><input name="radio" type="radio" id="op1" value="op1" checked="checked" />
+          <label for="op1">Nombre de Materia Prima<br />
+            <input type="radio" name="radio" id="op2" value="op2" />
+            Proveedor
+            <br />
+            <input type="radio" name="radio" id="op3" value="op3" />
+            Fecha
+            <br />
+          </label></td>
+      </tr>
+    </table></td>
+  </tr>
+</table>
+<form id="form1" name="form1" method="post" action=""><iframe src= index.php" width="820" height="200" scrolling="auto"></iframe>
+</form>
+</body>
+</html>
