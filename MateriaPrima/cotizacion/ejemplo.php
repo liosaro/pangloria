c2122b76dac7d1b16ed0aad262db6390a1be2948
Glenda Guerrero <glend.guerrero@catolica.edu.sv>
2013-05-31 21:37:51 -0600

    se modifico ingreso de cotizacion

--- MateriaPrima/cotizacion/ejemplo.php
+++ MateriaPrima/cotizacion/ejemplo.php
@@ -32,35 +32,51 @@ and open the template in the editor.
    $cmb_matpri .= '</select>';
 
 
+
+   $q_uni = "SELECT * FROM CATUNIDADES";
+   mysql_select_db($database_basepangloria, $basepangloria);
+   $res3 = mysql_query($q_uni, $basepangloria) or die(mysql_error());
+   //$row2 = mysql_fetch_assoc($res2);
+
+   $cmb_um = '<select id="unidad" name="unidad[]">';
+   while ($fila = mysql_fetch_assoc($res3)) {
+        $cmb_um .= '<option value="'.$fila['IDUNIDAD'].'">'.$fila['TIPOUNIDAD'].'</option>';
+   }
+   $cmb_um .= '</select>';
+
+
+
+
+
    $q_emp = "SELECT * FROM CATEMPLEADO";
    mysql_select_db($database_basepangloria, $basepangloria);
-   $res3 = mysql_query($q_emp, $basepangloria) or die(mysql_error());
+   $res4 = mysql_query($q_emp, $basepangloria) or die(mysql_error());
    //$row2 = mysql_fetch_assoc($res2);
 
    $cmb_emp = '<select id="empleado"  name="empleado">';
-   while ($fila = mysql_fetch_assoc($res3)) {
+   while ($fila = mysql_fetch_assoc($res4)) {
         $cmb_emp .= '<option value="'.$fila['IDEMPLEADO'].'">'.$fila['NOMBREEMPLEADO'].'</option>';
    }
    $cmb_emp .= '</select>';
 
     $q_prove = "SELECT * FROM CATPROVEEDOR";
    mysql_select_db($database_basepangloria, $basepangloria);
-   $res4 = mysql_query($q_prove, $basepangloria) or die(mysql_error());
+   $res5 = mysql_query($q_prove, $basepangloria) or die(mysql_error());
    //$row2 = mysql_fetch_assoc($res2);
 
    $cmb_prove = '<select id="proveedor"  name="proveedor">';
-   while ($fila = mysql_fetch_assoc($res4)) {
+   while ($fila = mysql_fetch_assoc($res5)) {
         $cmb_prove .= '<option value="'.$fila['IDPROVEEDOR'].'">'.$fila['NOMBREPROVEEDOR'].'</option>';
    }
    $cmb_prove .= '</select>';
 
      $q_vend = "SELECT * FROM CATVENDEDOR_PROV";
    mysql_select_db($database_basepangloria, $basepangloria);
-   $res5 = mysql_query($q_vend, $basepangloria) or die(mysql_error());
+   $res6 = mysql_query($q_vend, $basepangloria) or die(mysql_error());
    //$row2 = mysql_fetch_assoc($res2);
 
    $cmb_vend = '<select id="vendedor"  name="vendedor">';
-   while ($fila = mysql_fetch_assoc($res5)) {
+   while ($fila = mysql_fetch_assoc($res6)) {
         $cmb_vend .= '<option value="'.$fila['IDVENDEDOR'].'">'.$fila['NOM'].'</option>';
    }
    $cmb_vend .= '</select>';
@@ -68,11 +84,11 @@ and open the template in the editor.
 
      $q_tipop = "SELECT * FROM CATCONDICIONPAGO";
    mysql_select_db($database_basepangloria, $basepangloria);
-   $res6 = mysql_query($q_tipop, $basepangloria) or die(mysql_error());
+   $res7 = mysql_query($q_tipop, $basepangloria) or die(mysql_error());
    //$row2 = mysql_fetch_assoc($res2);
 
    $cmb_tipop = '<select id="condicion_pago"  name="condicion_pago">';
-   while ($fila = mysql_fetch_assoc($res6)) {
+   while ($fila = mysql_fetch_assoc($res7)) {
         $cmb_tipop .= '<option value="'.$fila['IDCONDICION'].'">'.$fila['TIPO'].'</option>';
    }
    $cmb_tipop .= '</select>';
@@ -106,7 +122,7 @@ and open the template in the editor.
 
                         coluna1 = linha.insertCell(2);
                         coluna1.align = "center";
-                        coluna1.innerHTML = '<td><<input type="text" name="um[]"></td>';
+                        coluna1.innerHTML = '<td><?php echo $cmb_um ?></td>';
 
                         coluna1 = linha.insertCell(3);
                         coluna1.align = "center";
@@ -181,13 +197,13 @@ and open the template in the editor.
                 <!--<td><input type="text" id="seq"  name="seq[]" readonly></td>-->
                 <!--<td><input type="text" id="mp"   name="mp[]"></td>-->
                 <td align="center"><?php echo $cmb_matpri ?></td>
-                <!--<td align="center"><?php echo $cmb_um ?></td> -->
+                <td align="center"><?php echo $cmb_um ?></td> 
                               
-                <td align="center"><input type="text" id="um"   name="um[]"></td>                <td align="center"><input type="text" id="qtde" name="qtde[]"></td>
+                <!--<td align="center"><input type="text" id="um"   name="um[]"></td>-->        <td align="center"><input type="text" id="qtde" name="qtde[]"></td>
                 <td align="center"><input type="text" id="pu"   name="pu[]"></td>
                 <td align="center"><input type="button" onClick="addLinha()" value="+"</td>
             </tr>
-        </table></br>
+         </table></br>
 
         <input type="submit" value="GUARDAR COTIZACION"> 
         </form> 
