8b5bc486a1e62b3d463053ce82537ab945408509
Ana Marielos Leal Acosta <lealmarielos@gmail.com>
2013-06-01 01:29:34 -0600

    eliminar sucursal

--- MateriaPrima/Sucursales/eliminacionSucur.php
+++ MateriaPrima/Sucursales/eliminacionSucur.php
@@ -1,3 +1,16 @@
+<head>
+<script language="JavaScript">
+function aviso(url){
+if (!confirm("ALERTA!! va a proceder a eliminar este registro, si desea eliminarlo de click en ACEPTAR\n de lo contrario de click en CANCELAR.")) {
+return false;
+}
+else {
+document.location = url;
+return true;
+}
+}
+</script>
+</head>
 <?php require_once('../../Connections/basepangloria.php'); ?>
 <?php
 if (!function_exists("GetSQLValueString")) {
@@ -31,84 +44,63 @@ function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDe
 }
 }
 
-$maxRows_elimiSucur = 10;
-$pageNum_elimiSucur = 0;
-if (isset($_GET['pageNum_elimiSucur'])) {
-  $pageNum_elimiSucur = $_GET['pageNum_elimiSucur'];
+$maxRows_elimisucur = 10;
+$pageNum_elimisucur = 0;
+if (isset($_GET['pageNum_elimisucur'])) {
+  $pageNum_elimisucur = $_GET['pageNum_elimisucur'];
 }
-$startRow_elimiSucur = $pageNum_elimiSucur * $maxRows_elimiSucur;
+$startRow_elimisucur = $pageNum_elimisucur * $maxRows_elimisucur;
 
 mysql_select_db($database_basepangloria, $basepangloria);
-$query_elimiSucur = "SELECT * FROM CATSUCURSAL ORDER BY IDSUCURSAL ASC";
-$query_limit_elimiSucur = sprintf("%s LIMIT %d, %d", $query_elimiSucur, $startRow_elimiSucur, $maxRows_elimiSucur);
-$elimiSucur = mysql_query($query_limit_elimiSucur, $basepangloria) or die(mysql_error());
-$row_elimiSucur = mysql_fetch_assoc($elimiSucur);
+$query_elimisucur = "SELECT * FROM CATSUCURSAL ORDER BY IDSUCURSAL ASC";
+$query_limit_elimisucur = sprintf("%s LIMIT %d, %d", $query_elimisucur, $startRow_elimisucur, $maxRows_elimisucur);
+$elimisucur = mysql_query($query_limit_elimisucur, $basepangloria) or die(mysql_error());
+$row_elimisucur = mysql_fetch_assoc($elimisucur);
 
-if (isset($_GET['totalRows_elimiSucur'])) {
-  $totalRows_elimiSucur = $_GET['totalRows_elimiSucur'];
+if (isset($_GET['totalRows_elimisucur'])) {
+  $totalRows_elimisucur = $_GET['totalRows_elimisucur'];
 } else {
-  $all_elimiSucur = mysql_query($query_elimiSucur);
-  $totalRows_elimiSucur = mysql_num_rows($all_elimiSucur);
+  $all_elimisucur = mysql_query($query_elimisucur);
+  $totalRows_elimisucur = mysql_num_rows($all_elimisucur);
 }
-$totalPages_elimiSucur = ceil($totalRows_elimiSucur/$maxRows_elimiSucur)-1;
+$totalPages_elimisucur = ceil($totalRows_elimisucur/$maxRows_elimisucur)-1;
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Documento sin título</title>
-<script language="JavaScript">
-function aviso(url){
-if (!confirm("ALERTA!! va a proceder a eliminar este registro, si desea eliminarlo de click en ACEPTAR\n de lo contrario de click en CANCELAR.")) {
-return false;
-}
-else {
-document.location = url;
-return true;
-}
-}
-</script>
-</head>
-
-<body>
-<table border="1">
-  <tr></tr>
-<tr>
-  <td colspan="5" align="center" bgcolor="#999999"><h1>Eliminar Sucursal</h1></td>
-</tr>
-<tr>
-  <td colspan="5"><p>
-    <iframe src="filtroelimiSucur.php" name="eliminar" width="820" height="400" scrolling="Auto" frameborder="0" id="eliminar"></iframe>
-  </p>
-    <p><img src="../../imagenes/icono/Back-32.png" width="32" height="32" /><img src="../../imagenes/icono/Backward-32.png" width="32" height="32" /><img src="../../imagenes/icono/Forward-32.png" width="32" height="32" /><img src="../../imagenes/icono/Next-32.png" width="32" height="32" /></p></td>
-</tr>
-<tr>
-  <td colspan="5"><form action="filtroelimiSucur.php" method="post" name="enviosucur" target="eliminar" id="enviosucur">
-    <label for="elimisucur"></label>
-    <input type="text" name="elimisucur" id="elimisucur" />
-    <input type="submit" name="button" id="button" value="Enviar" />
-  </form></td>
-</tr>
-<tr>
-  <td>Eliminacion</td>
-  <td>IDSUCURSAL</td>
-  <td>NOMBRESUCURSAL</td>
-  <td>DIRECCIONSUCURSAL</td>
-  <td>TELEFONOSUCURSAL</td>
-</tr>
-<?php do { ?>
-<tr>
-  <td><a href="javascript:;" onclick="aviso('eliminarSucur.php?root=<?php echo $row_elimiSucur['IDSUCURSAL']; ?>'); return false;">Eliminar</a></td>
-  <td><?php echo $row_elimiSucur['IDSUCURSAL']; ?></td>
-  <td><?php echo $row_elimiSucur['NOMBRESUCURSAL']; ?></td>
-  <td><?php echo $row_elimiSucur['DIRECCIONSUCURSAL']; ?></td>
-  <td><?php echo $row_elimiSucur['TELEFONOSUCURSAL']; ?></td>
-</tr>
-<?php } while ($row_elimiSucur = mysql_fetch_assoc($elimiSucur)); ?>
+<table width="820" border="0">
+  <tr>
+    <td><iframe src="filtroelimiSucur.php" name="conten" width="820" height="400" scrolling="auto" id="conten"></iframe>&nbsp;</td>
+  </tr>
+  <tr>
+    <td><img src="../../imagenes/icono/Back-32.png" width="32" height="32" /><img src="../../imagenes/icono/Backward-32.png" width="32" height="32" /><img src="../../imagenes/icono/Forward-32.png" width="32" height="32" /><img src="../../imagenes/icono/Next-32.png" width="32" height="32" /></td>
+  </tr>
+  <tr>
+    <td><form action="filtroelimiSucur.php" method="post" name="form1" target="conten" id="form1">
+      <label for="filtrosucu"></label>
+      <input type="text" name="filtrosucu" id="filtrosucu" />
+      <input type="submit" name="button" id="button" value="Enviar" />
+    </form></td>
+  </tr>
+  <tr>
+    <td>&nbsp;
+      <table border="1">
+        <tr>
+          <td>Eliminarcion</td>
+          <td>IDSUCURSAL</td>
+          <td>NOMBRESUCURSAL</td>
+          <td>DIRECCIONSUCURSAL</td>
+          <td>TELEFONOSUCURSAL</td>
+        </tr>
+        <?php do { ?>
+          <tr>
+            <td><a href="javascript:;" onclick="aviso('eliminarSucur.php?root=<?php echo $row_elimisucur['IDSUCURSAL']; ?>'); return false;">Eliminar</a></td>
+            <td><?php echo $row_elimisucur['IDSUCURSAL']; ?></td>
+            <td><?php echo $row_elimisucur['NOMBRESUCURSAL']; ?></td>
+            <td><?php echo $row_elimisucur['DIRECCIONSUCURSAL']; ?></td>
+            <td><?php echo $row_elimisucur['TELEFONOSUCURSAL']; ?></td>
+          </tr>
+          <?php } while ($row_elimisucur = mysql_fetch_assoc($elimisucur)); ?>
+    </table></td>
+  </tr>
 </table>
-<p>&nbsp;</p>
-</body>
-</html>
 <?php
-mysql_free_result($elimiSucur);
+mysql_free_result($elimisucur);
 ?>
